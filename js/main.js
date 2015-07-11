33d9e21a4b1b7a36477e07403831ba0388359cb1
Matt Greensmith <matt.greensmith@gmail.com>
2015-07-10 19:33:55 -0700

    looks like Jenkins wallboard now

--- js/main.js
+++ js/main.js
@@ -1,21 +1,12 @@
 function buildGroup(group) {
-  $( "#badges" ).append('<ul class="list-group row">');
-
   $.each( group.pipelines, function(i , val) {
     btn_class = val.instances[0].latest_stage_state == 'Passed' ? 'btn-success' : 'btn-danger';
-    $( "#badges" ).append('<li class="list-group-item col-xs-6"><button type="button" class="btn btn-lg ' + btn_class + '">' + val.name + '</button></li>')
+    $( "#badges" ).append('<li class="col-xs-6"><button type="button" class="btn btn-lg ' + btn_class + '">' + val.name + '</button></li>')
   });
-
-  $( "#badges" ).append('<ul class="list-group row">');
 }
 
 $.getJSON( "http://10.0.32.27/go/dashboard.json", function( data ) {
   $.each( data, function(i , val) {
     buildGroup(val);
   });
-
-//  $('#badges').freetile(({
-//   animate: true,
-//    elementDelay: 3
-//  }));
 });
