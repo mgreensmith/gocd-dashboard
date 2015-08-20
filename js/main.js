08c8d757223bf555ea7b15e22349810f3caace1e
Matt Greensmith <matt.greensmith@gmail.com>
2015-08-20 14:25:13 -0700

    accept server and group parameters, buttons link to builds, autorefresh

--- js/main.js
+++ js/main.js
@@ -1,12 +1,39 @@
 function buildGroup(group) {
   $.each( group.pipelines, function(i , val) {
     btn_class = val.instances[0].latest_stage_state == 'Passed' ? 'btn-success' : 'btn-danger';
-    $( "#badges" ).append('<li class="col-xs-6"><button type="button" class="btn btn-lg ' + btn_class + '">' + val.name + '</button></li>')
+    link = 'http://' + query.server + '/go/tab/pipeline/history/' + val.name;
+    $( "#badges" ).append('<li class="col-xs-6"><a href="' + link + '" target="_blank" class="btn btn-lg ' + btn_class + '">' + val.name + '</button></li>')
   });
 }
 
-$.getJSON( "http://10.0.32.27/go/dashboard.json", function( data ) {
-  $.each( data, function(i , val) {
-    buildGroup(val);
+function queryParse(querystring) {
+  var result = {};
+  (querystring || '').replace(
+      new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+      function($0, $1, $2, $3) {
+          result[$1] = $3 !== undefined ? decodeURIComponent($3) : $3;
+      }
+  );
+  return result;
+}
+
+var query = queryParse( window.location.search );
+
+if ( query.server ) {
+
+  $.getJSON( "http://" + query.server + "/go/dashboard.json", function( data ) {
+
+    //var query = queryParse( window.location.search );
+
+    if ( query.group ) {
+      buildGroup( _.find(data, { 'name': _.trim(query.group, '/' ) } ));
+    } else {
+      $.each( data, function(i , val) {
+        buildGroup(val);
+      });
+    };
   });
-});
+
+} else {
+  $('#usage').toggle();
+}
